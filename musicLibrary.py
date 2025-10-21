642de7e41b3609232a5533b739ab820faa1e6a74
Swastika Singh <ssingh36_be24@thapar.edu>
2025-10-21 14:07:45 +0530

    Create musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music={
+    "Opalite by taylor swift Mv":"https://www.youtube.com/watch?v=3PJC6uPIekc&list=RD3PJC6uPIekc&start_radio=1",
+    "Hello by Adele MV":"https://www.youtube.com/watch?v=YQHsXMglC9A&list=RDYQHsXMglC9A&start_radio=1&pp=ygULaGVsbG8gYWRlbGWgBwE%3D",
+    "Tujhe sochta hun by KK spotify":"https://open.spotify.com/track/47l2Dd3XPBLio3kjbEMyDJ?si=4ea711a2f4e6486a",
+    "O mere dil ke Chain spotify":"https://open.spotify.com/track/6Frhkb7giXWjeJAX2dJT88?si=e62225f3720549c6"
+}
